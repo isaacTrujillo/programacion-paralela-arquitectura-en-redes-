690bc0ca001a8538251e9beffc0aaacdc5ba1475
isaacTrujillo <isaac_filos2000@outlook.com>
2018-02-10 20:53:55 -0600

    ceasars_crypt function added

--- practica1/practica1.rb
+++ practica1/practica1.rb
@@ -1,3 +1,13 @@
+def ceasars_crypt(string)
+  string.gsub!(/[^a-zA-Z]/, '')
+  string = string.upcase
+  string.each_char.with_index do |char, index|
+    current = char.ord + 13
+    string[index] = (current > 90 ? 64 + (current - 90) : current).chr
+  end
+  string
+end
+
 def palindrome?(text)
   return false unless text.respond_to? :each_char
   second = text.size - 1
@@ -19,5 +29,8 @@ def sum_squares(squares)
 end
 
 puts sum_squares([1, 2, 3])
-puts palindrome? 'HolaaloH'
-puts palindrome? 'Logan'
+puts palindrome? 'abcdcba'
+puts palindrome? 'abcddcba'
+puts palindrome? 'nope'
+puts ceasars_crypt 'hola mundo!!'
+puts ceasars_crypt ceasars_crypt('hola mundo!!')
