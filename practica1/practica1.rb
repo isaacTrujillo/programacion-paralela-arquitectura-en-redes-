8b1da10b332348547de5b137577913a2a84daf8b
isaacTrujillo <isaac_filos2000@outlook.com>
2018-02-10 19:53:19 -0600

    palindrome? function added

--- practica1/practica1.rb
+++ practica1/practica1.rb
@@ -1,3 +1,14 @@
+def palindrome?(text)
+  return false unless text.respond_to? :each_char
+  second = text.size - 1
+  text.each_char.with_index do |char, index|
+    return false unless char == text[second]
+    second -= 1
+    break if second <= index
+  end
+  true
+end
+
 def sum_squares(squares)
   return false unless squares.respond_to? :each
   total = 0
@@ -8,3 +19,5 @@ def sum_squares(squares)
 end
 
 puts sum_squares([1, 2, 3])
+puts palindrome? 'HolaaloH'
+puts palindrome? 'Logan'
