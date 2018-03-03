7e2ae444474f49c2bc0bb2267b3c6af48801dd7d
isaacTrujillo <isaac_filos2000@outlook.com>
2018-03-03 14:15:11 -0600

    users controller created

---
+++ practica_4/controllers/users.rb
@@ -0,0 +1,27 @@
+# product class Users for factory class CRUD
+class Users
+  def initialize(dataset)
+    @dataset = dataset
+  end
+
+  def create(data)
+    @dataset.insert(name: data[:name], userName: data[:userName],
+                    email: data[:email])
+  end
+
+  def by_id(id)
+    @dataset.where(id: id).first
+  end
+
+  def list_all
+    @dataset.all
+  end
+
+  def delete(id)
+    @dataset.where(id: id).delete
+  end
+
+  def self.table
+    :users
+  end
+end
