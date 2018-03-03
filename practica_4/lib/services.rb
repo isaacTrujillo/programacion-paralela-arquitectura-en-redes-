aee5bae5102b9235d5120a2493b7c0668c8346c3
isaacTrujillo <isaac_filos2000@outlook.com>
2018-03-03 14:13:03 -0600

    Services modified to load configuration

--- practica_4/lib/services.rb
+++ practica_4/lib/services.rb
@@ -1,1 +1,18 @@
-# TODO: Implement this to connect to database
+require 'safe_yaml'
+require 'sequel'
+
+class Service
+
+  def self.configure!
+    load_config
+  end
+
+  def self.load_config!
+    SafeYAML::OPTIONS[:default_mode] = :safe
+    @config = SafeYAML.load_file('./config/config.yaml')
+  end
+
+  def self.database
+    @database ||= Sequel.connect(@config['database']['endpoint'])
+  end
+end
